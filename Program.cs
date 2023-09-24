42aabca38f37e3bf1cb62ba761850ac537b36b01
Yohan_Garcia <75887329+yohanJS@users.noreply.github.com>
2023-09-24 09:59:16 -0400

    - implemented initial email confirmation sender

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using PocMVCApp.Data;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using PocMVCApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +15,9 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddTransient<IEmailSender, EmailSender>();
+builder.Services.Configure<AuthMessageSenderOptions>(builder.Configuration);
 
 var app = builder.Build();
 
---
+++ Services/AuthMessageSenderOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace PocMVCApp.Services
+{
+    public class AuthMessageSenderOptions
+    {
+        public string? SendGridKey { get; set; }
+    }
+}
