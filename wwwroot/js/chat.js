c830376b5095c28654eb76f22323e6948a835752
Yohan_Garcia <75887329+yohanJS@users.noreply.github.com>
2023-09-25 10:49:20 -0400

    - fixed alignment issues

--- wwwroot/js/chat.js
+++ wwwroot/js/chat.js
@@ -8,10 +8,7 @@ document.getElementById("sendButton").disabled = true;
 connection.on("ReceiveMessage", function (user, message) {
     var li = document.createElement("li");
     document.getElementById("messagesList").appendChild(li);
-    // We can assign user-supplied strings to an element's textContent because it
-    // is not interpreted as markup. If you're assigning in any other way, you 
-    // should be aware of possible script injection concerns.
-    li.innerHTML = `${message}`;
+    li.innerHTML = `${user} <br> ${message}`;
 });
 
 connection.start().then(function () {
