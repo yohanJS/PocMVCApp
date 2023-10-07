a6f3eda8116ae58aff028140373cb1d69dea7ac9
Yohan Garcia <yoanvaldes03@gmail.com>
2023-10-07 13:24:11 -0400

    - removed the phone number as a property of the UserPrimaryInfo model

--- Entities/UserPrimaryInfo.cs
+++ Entities/UserPrimaryInfo.cs
@@ -12,7 +12,6 @@ namespace MvcAppPOC.Entities
         public int Age { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
-        public string? PhoneNumber { get; set; }
         public string? Address { get; set; }
         public string? ZipCode { get; set; }
 
---
+++ Migrations/20231007172312_RemovingPhoneNumberFromUserPrimaryInfo.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PocMVCApp.Migrations
+{
+    public partial class RemovingPhoneNumberFromUserPrimaryInfo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "UserPrimaryInfo");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "UserPrimaryInfo",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
